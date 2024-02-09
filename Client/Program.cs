f73bd9fdcd151e0ee6e3ef649499008af82f143e
spcii007 <pablo.cifuentes@spcii.com>
2024-02-08 22:42:24 -0500

    Restore old site

---
+++ Client/Program.cs
@@ -0,0 +1,21 @@
+using SPCII_APP.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>();
+
+app.Run();
